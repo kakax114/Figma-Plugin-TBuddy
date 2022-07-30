cc3a83cd576378be4b55d89a85ff75ff32583a4c
-CX- <nz.carterxiao@gmail.com>
2022-07-31 11:36:11 +1200

    figma plugin with react

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,46 +1,97 @@
-import * as React from 'react';
-import '../styles/ui.css';
+import React, {useState, useEffect} from 'react';
+// import '../styles/ui.css';
 
-declare function require(path: string): any;
+const App = () => {
+    const [info, setInfo] = useState('');
+    const [inputText, setInputText] = useState('');
+    const [items, setItems] = useState([]);
+    const [radioState, setRadioState] = useState('tableByRow');
 
-const App = ({}) => {
-    const textbox = React.useRef<HTMLInputElement>(undefined);
-
-    const countRef = React.useCallback((element: HTMLInputElement) => {
-        if (element) element.value = '5';
-        textbox.current = element;
-    }, []);
-
-    const onCreate = () => {
-        const count = parseInt(textbox.current.value, 10);
-        parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
-    };
-
-    const onCancel = () => {
-        parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
-    };
-
-    React.useEffect(() => {
+    useEffect(() => {
         // This is how we read messages sent from the plugin controller
         window.onmessage = (event) => {
             const {type, message} = event.data.pluginMessage;
-            if (type === 'create-rectangles') {
-                console.log(`Figma Says: ${message}`);
+            if (type === 'create-table') {
+                // setInfo('Message from figma: ' + message.length);
             }
         };
     }, []);
 
+    useEffect(() => {
+        if (radioState === 'tableByRow') {
+            tableByRow();
+        } else {
+            tableByColumn();
+        }
+    }, [inputText]);
+
+    const tableByRow = () => {
+        setItems(inputText.split('\n').map((item) => item.split('\t')));
+    };
+
+    const tableByColumn = () => {
+        setItems(transposeArray(inputText.split('\n').map((item) => item.split('\t'))));
+    };
+
+    //function of tableByColumn
+    const transposeArray = (arr) => {
+        let newItems = [];
+        for (let i = 0; i < arr[0].length; i++) {
+            let newItem = [];
+            for (let j = 0; j < arr.length; j++) {
+                newItem.push(arr[j][i]);
+            }
+            newItems.push(newItem);
+        }
+        return newItems;
+    };
+
+    const onTable = () => {
+        parent.postMessage(
+            {
+                pluginMessage: {
+                    type: 'create-table',
+                    items,
+                    state: radioState,
+                },
+            },
+            '*'
+        );
+    };
+
     return (
         <div>
-            <img src={require('../assets/logo.svg')} />
-            <h2>Rectangle Creator</h2>
-            <p>
-                Count: <input ref={countRef} />
-            </p>
-            <button id="create" onClick={onCreate}>
-                Create
-            </button>
-            <button onClick={onCancel}>Cancel</button>
+            {/* <img src={require('../assets/logo.svg')} /> */}
+            <h2>Hello World</h2>
+            <textarea value={inputText} onChange={(e) => setInputText(e.target.value)} />
+            {/* radio button of 2 options, tabeByRow and tableByColumn, default selected to tableByRow */}
+            <div>
+                <input
+                    type="radio"
+                    name="radio"
+                    value="tableByRow"
+                    checked={radioState === 'tableByRow'}
+                    onChange={() => {
+                        tableByRow();
+                        setRadioState('tableByRow');
+                    }}
+                />
+                <label>Autolayout by Row</label>
+                <input
+                    type="radio"
+                    name="radio"
+                    value="tableByColumn"
+                    checked={radioState === 'tableByColumn'}
+                    onChange={() => {
+                        tableByColumn();
+                        setRadioState('tableByColumn');
+                    }}
+                />
+                <label>by Column</label>
+            </div>
+
+            <button onClick={onTable}>Create table</button>
+            <p>{info}</p>
         </div>
     );
 };
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,26 +1,114 @@
 figma.showUI(__html__);
 
 figma.ui.onmessage = (msg) => {
-    if (msg.type === 'create-rectangles') {
-        const nodes = [];
+    if (msg.type === 'create-table') {
+        //listening on 'Create Table' button pressed
+        const input = [
+            [1, 2],
+            [3, 4],
+            [5, 6],
+            [7, 8],
+        ];
+        const arr = [];
+        // const input = msg.items
 
-        for (let i = 0; i < msg.count; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
+        // console.log(msg.state)
+        if (msg.state === 'tableByRow') {
+            autoByRow(input, arr);
+            console.log('autobyrow');
+        } else if (msg.state === 'tableByColumn') {
+            autoByCol(input, arr);
+            console.log('autobycol');
         }
 
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
-
-        // This is how figma responds back to the ui
         figma.ui.postMessage({
-            type: 'create-rectangles',
-            message: `Created ${msg.count} Rectangles`,
+            type: 'create-table',
+            message: msg.items,
         });
     }
+};
 
-    figma.closePlugin();
+const autoByRow = (input, arr) => {
+    const mainFrame = figma.createFrame();
+    mainFrame.layoutMode = 'VERTICAL';
+    mainFrame.counterAxisSizingMode = 'AUTO';
+    getRow(input, arr).forEach((row) => {
+        mainFrame.appendChild(row);
+    });
+};
+
+const autoByCol = (input, arr) => {
+    const mainFrame = figma.createFrame();
+    mainFrame.layoutMode = 'HORIZONTAL';
+    mainFrame.counterAxisSizingMode = 'AUTO';
+    getCol(input, arr).forEach((col) => {
+        mainFrame.appendChild(col);
+    });
+};
+
+//return a list with frames
+const getRow = (input, row) => {
+    for (let i = 0; i < input.length; i++) {
+        const col = [];
+        for (let j = 0; j < input[i].length; j++) {
+            col.push(getText(input[i][j]));
+        }
+        row.push(getColFirst(col));
+    }
+
+    return row;
+};
+
+const getCol = (input, col) => {
+    for (let i = 0; i < input.length; i++) {
+        const row = [];
+        for (let j = 0; j < input[i].length; j++) {
+            row.push(getText(input[i][j]));
+        }
+        col.push(getRowFirst(row));
+    }
+    return col;
+};
+
+//return a frame with some texts
+const getColFirst = (arr) => {
+    const frame = figma.createFrame();
+    Promise.all(arr).then((nodes) => {
+        // console.log(nodes);
+
+        frame.layoutMode = 'HORIZONTAL';
+        nodes.forEach((node) => {
+            frame.appendChild(node);
+        });
+        frame.counterAxisSizingMode = 'AUTO';
+        // figma.currentPage.selection = nodes;
+        // figma.viewport.scrollAndZoomIntoView(nodes);
+    });
+    return frame;
+};
+
+const getRowFirst = (arr) => {
+    const frame = figma.createFrame();
+    Promise.all(arr).then((nodes) => {
+        // console.log(nodes);
+
+        frame.layoutMode = 'VERTICAL';
+        nodes.forEach((node) => {
+            frame.appendChild(node);
+        });
+        frame.counterAxisSizingMode = 'AUTO';
+    });
+    return frame;
+};
+
+// figma.closePlugin();
+
+//return a text
+const getText = async (i) => {
+    const text = figma.createText();
+    await figma.loadFontAsync(text.fontName);
+    text.characters = i.toString();
+    // text.fontSize = 18
+    text.fills = [{type: 'SOLID', color: {r: 1, g: 0, b: 0}}];
+    return text;
 };
